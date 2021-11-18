ea487fe87acf3c2a05bd1e39cd89a8668d396db7
nxm-xqxx <89255691+nxm-xqxx@users.noreply.github.com>
2021-11-18 11:54:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -162,8 +162,8 @@ def event_handle(event):
             replyObj = TextSendMessage(text="อย่างจ๊าบเลยคับบ")
             line_bot_api.reply_message(rtoken, replyObj)
         else :
-            headers = request.headers
-            json_headers = json.dumps({k:v for k, v in headers.items()})
+            #headers = request.headers
+            #json_headers = json.dumps({k:v for k, v in headers.items()})
             '''
             json_line = request.get_json(force=False,cache=False)
             json_line = json.dumps(json_line)
